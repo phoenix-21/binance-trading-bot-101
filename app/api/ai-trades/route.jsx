98dd0e63f19155276fa54f0774660381657c6fb9
phoenix-21 <lunardrift2@gmail.com>
2025-10-15 11:03:11 +0500

    Update route.jsx

--- app/api/ai-trades/route.jsx
+++ app/api/ai-trades/route.jsx
@@ -1,7 +1,6 @@
 // app/api/ai-trades/route.js
 import { Pool } from "pg";
 import moment from "moment-timezone";
-import 'dotenv/config';
 
 const POSTGRES_URL = process.env.POSTGRES_URL;
 const pool = new Pool({ connectionString: POSTGRES_URL });
